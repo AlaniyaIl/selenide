adb0bccd335ae3613db25c111b597ed07d5bf78f
Ilya <alaniyailya@bk.ru>
2023-03-14 22:10:51 +0300

    first commit

--- src/test/java/ru/netology/CardFormTest.java
+++ src/test/java/ru/netology/CardFormTest.java
@@ -1,11 +1,8 @@
 package ru.netology;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.FileDownloadMode;
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import org.testng.annotations.BeforeSuite;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -17,14 +14,14 @@ import static com.codeborne.selenide.Selenide.*;
 
  class CardFormTest {
 
-    String generateDate(int days) {
-        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    }
+    String generateTest(int days) {
+         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+     }
 
 
     @Test
-    void positiveFormTest() {
-        String date = generateDate(1);
+    void positiveTest() {
+        String date = generateTest(10);
         open("http://localhost:9999");
         $("[data-test-id= city] input").setValue("Москва");
         $("[data-test-id= date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
@@ -33,13 +30,14 @@ import static com.codeborne.selenide.Selenide.*;
         $("[data-test-id= phone] input").setValue("+79012345678");
         $("[data-test-id= agreement] ").click();
         $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id=notification]").shouldHave(exactText("Успешно! " + "Встреча успешно забронирована на " + date), Duration.ofSeconds(15)).shouldBe(visible);
+        $("[data-test-id=notification]").shouldHave(exactText("Успешно! " + "Встреча успешно забронирована на "
+                + date), Duration.ofSeconds(15)).shouldBe(visible);
     }
 
 
     @Test
-     void dropDownTest() {
-        String date = generateDate(1);
+     void dropTest() {
+        String date = generateTest(10);
         open("http://localhost:9999");
         $("[data-test-id= city] input").setValue("Москва");
         $("[data-test-id= date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
@@ -48,6 +46,7 @@ import static com.codeborne.selenide.Selenide.*;
         $("[data-test-id= phone] input").setValue("+79012345678");
         $("[data-test-id= agreement] ").click();
         $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id=notification]").shouldHave(exactText("Успешно! " + "Встреча успешно забронирована на " + date), Duration.ofSeconds(15)).shouldBe(visible);
+        $("[data-test-id=notification]").shouldHave(exactText("Успешно! " + "Встреча успешно забронирована на "
+                        + date),Duration.ofSeconds(19)).shouldBe(visible);
     }
 }
